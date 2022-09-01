b1b441bbf44711655d1b79043e4203dc7c1201a5
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-09-01 20:25:50 +0300

    minor change on the local host

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,21 @@
-let inmateAPI='http://localhost:3000/inmates';
+let inmateAPI='http://localhost:3000/inmates/5664';
+async function getDetails(){
+        const response=await fetch(inmateAPI);
+        const data=await response.json();
+        const {name,Bond,Hearings,Release_Date,criminal_Offense}=data;
+        document.getElementById("Nameinmate").textContent=name;
+        document.getElementById("inmatebond").textContent=Bond;
+        document.getElementById("inmatehearing").textContent=Hearings;
+        document.getElementById("release_date").textContent=Release_Date;
+        document.getElementById("criminal_offense").textContent=criminal_Offense;
+    }
+    document.getElementById("submit").addEventListener('click',getDetails)
+    // document.getElementById("submiting").addEventListener('click',
+    // )
+    function checking(){
+            alert("kindly choose the prison you want to search from")
+        }
 
-// fetch(inmateAPI)
-// .then(response=>{
-//     console.log(response);
-//     return(response.text()).then(response=>{
-//         console.log(response)
-//     })
-// })
-document.getElementById("submit").addEventListener('click',()=>{
-    fetch(inmateAPI)
-    .then(response=>response.text())
-    .then(response=>{
-        console.log(response)
-        document.getElementById("inmateDetails").append(response)
-    })
-})
+
 
 
